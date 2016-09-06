aba1bf64ea26055d35f1ab3dd2d174f227ab1f8f
Thant Thet <t@ttkz.me>
2016-09-06 18:38:05 +0630

    fix makefile syntax

--- rtbkit/openrtb/openrtb.mk
+++ rtbkit/openrtb/openrtb.mk
@@ -1,1 +1,1 @@
-$(eval $(call library,openrtb,openrtb_parsing.cc openrtb.cc,openrtb_native_parsing.cc,openrtb_native.cc,value_description))
+$(eval $(call library,openrtb,openrtb_parsing.cc openrtb.cc openrtb_native_parsing.cc openrtb_native.cc,value_description))
